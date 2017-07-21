fefb0d3d627a2e3396bbdde52919356a570e89a4
vinod2106 <vinod254581@gmail.com>
2017-07-21 14:48:54 +0200

    Initial commit

---
+++ src/main/java/com/hbase/HbaseEventSerializer.java
@@ -0,0 +1,45 @@
+package com.hbase;
+
+
+import java.util.List;
+
+import org.apache.flume.Event;
+import org.apache.flume.conf.Configurable;
+import org.apache.flume.conf.ConfigurableComponent;
+import org.apache.hadoop.hbase.client.Increment;
+import org.apache.hadoop.hbase.client.Row;
+
+/**
+ * Interface for an event serializer which serializes the headers and body
+ * of an event to write them to hbase. This is configurable, so any config
+ * params required should be taken through this. Only the column family is
+ * passed in. The columns should exist in the table and column family
+ * specified in the configuration for the HbaseSink.
+ *
+ */
+public interface HbaseEventSerializer extends Configurable,
+        ConfigurableComponent {
+    /**
+     * Initialize the event serializer.
+     */
+    public void initialize(Event event, byte[] columnFamily);
+
+    /**
+     * Get the actions that should be written out to hbase as a result of this
+     * event. This list is written to hbase using the HBase batch API.
+     * @return List of {@link org.apache.hadoop.hbase.client.Row} which
+     * are written as such to HBase.
+     *
+     * 0.92 increments do not implement Row, so this is not generic.
+     *
+     */
+    public List<Row> getActions();
+
+    public List<Increment> getIncrements();
+    /*
+     * Clean up any state. This will be called when the sink is being stopped.
+     */
+    public void close();
+
+
+}
---
+++ src/main/java/com/hbase/SimpleRowKeyGenerator.java
@@ -0,0 +1,41 @@
+package com.hbase;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+import java.util.UUID;
+/**
+ * Utility class for users to generate their own keys. Any key can be used,
+ * this is just a utility that provides a set of simple keys.
+ *
+ *
+ */
+public class SimpleRowKeyGenerator {
+
+    public static byte[] getUUIDKey(String prefix)
+            throws UnsupportedEncodingException{
+        return (prefix + UUID.randomUUID().toString()).getBytes("UTF8");
+    }
+
+    public static byte[] getRandomKey(String prefix)
+            throws UnsupportedEncodingException{
+        return (prefix + String.valueOf(new Random().nextLong())).getBytes("UTF8");
+    }
+    public static byte[] getTimestampKey(String prefix)
+            throws UnsupportedEncodingException {
+        return (prefix + String.valueOf(
+                System.currentTimeMillis())).getBytes("UTF8");
+    }
+    public static byte[] getNanoTimestampKey(String prefix)
+            throws UnsupportedEncodingException{
+        return (prefix + String.valueOf(
+                System.nanoTime())).getBytes("UTF8");
+
+    }
+    //custom key by using first 2 fields
+    public static byte[] getBDMPKey(byte[] payload, String delimit)
+            throws UnsupportedEncodingException{
+       String[] splitRec =  String.valueOf(payload).split(delimit);
+        return (splitRec[0]+ "-"+ splitRec[1]).getBytes("UTF8");
+    }
+
+
+}
